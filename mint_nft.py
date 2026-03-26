968f121d327f7f76e70a9bc7d750830b93ae571c
Karath-Vamsi <ugs22047_aids.vamsi@cbit.org.in>
2026-03-26 22:56:18 +0530

    minor bug fix

--- mint_nft.py
+++ mint_nft.py
@@ -6,12 +6,10 @@ from pathlib import Path
 
 load_dotenv()
 
-# Connect
 w3 = Web3(Web3.HTTPProvider(os.getenv("RPC_URL")))
 account = w3.to_checksum_address(os.getenv("USER_WALLET_ADDRESS"))
 private_key = os.getenv("USER_PRIVATE_KEY")
 
-# Get ABI
 ABI_PATH = Path("marketplace/artifacts/contracts/ExecutiveNFT.sol/ExecutiveNFT.json")
 with open(ABI_PATH) as f:
     abi = json.load(f)["abi"]
@@ -20,7 +18,7 @@ contract = w3.eth.contract(address=os.getenv("CONTRACT_ADDRESS"), abi=abi)
 
 print(f"Minting NFT for {account}...")
 
-# Build Transaction
+# Building the Transaction
 nonce = w3.eth.get_transaction_count(account)
 tx = contract.functions.mintTwin().build_transaction({
     'from': account,
@@ -29,7 +27,8 @@ tx = contract.functions.mintTwin().build_transaction({
     'gasPrice': w3.to_wei('50', 'gwei')
 })
 
-# Sign and Send
+# Signing and Sending Tx. index to Hardhat node
+# Note to self: Store Tx. hash to IPFS
 signed_tx = w3.eth.account.sign_transaction(tx, private_key)
 tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
 
